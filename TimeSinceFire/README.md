f504c8538061c3eec9653f64e3606892484f7f18
pvn-a <pavan.leo36dna@gmail.com>
2024-12-19 14:06:03 -0700

    add a non interactive python script and update readme

--- TimeSinceFire/README.md
+++ TimeSinceFire/README.md
@@ -1,25 +1,34 @@
 # Time Since Fire
 
-You will need python, jupyter notebook to run this.   
-*You will need to create an accout with Google Earth Engine and create a project and get the project id.*  
-Unfortunately, running this code just through a plain python script will not give us the capabilities of rendering the map. Hence we must use jupyter notebooks.
+## Aim
+Through this code we aim to analyse a region on earth between a certain timeframe for the most recent occurences of fires. For example consider these values - start_date is 2000-01-01 and end_date is 2024-11-30 and the region of interest is Southern California. We want to analyze and visualize all the areas of Southern California for fire events during those years. We want to compute the exact date on which a recent fire was observed and the number of days since end_date a fire was seen back in time.
+
+You will need python and jupyter notebook to run this.   
+*You will need to create an accout with Google Earth Engine, create a project and get the project id.*  
+Unfortunately, running this code just through a plain python script will not give us the capabilities of rendering the map. Hence we suggest using jupyter notebooks.
 
 ## Steps to run this code
 
-This code needs you to have an account with Google Earth Engine. It will avoid the hassles of downloading the images on local and processing it.
+This code needs you to have an account with Google Earth Engine.    
+It will avoid the hassles of downloading the images on local and processing it.  
+Please setup the virtual env and install the dependencies, then run either of method 1 (preferred) or method 2.
 
 ### Setup the virtual env
 ```sh
 cd TimeSinceFire
-python3 -m venv env #create a python virtual environment
-source env/bin/activate
+python3 -m venv fire #create a python virtual environment
+source fire/bin/activate
 pip3 install -r requirements.txt
 ```
 
-### Run the python notebook
+### Method 1 (Preferred) - Run the python notebook
+This method gives you an interactive map, that displays the latitude, longitude, date of fire in epoch format(most recent burn date) and the number of days before the end_date the fire occured (Days since Last Burn).
 ```sh
 jupyter notebook
+#<open method1_sce_timesincefire.ipynb>
 ```
+It will ask you for authentication with your Google Account. Please complete that step.
+
 In the last cell of the jupyter notebook make the necessary changes i.e
 ```
 initialize_ee - add the google earth engine project id here
@@ -30,8 +39,14 @@ center - just focuses the map on this coordinate
 palette - choose your colors for showing on the map
 ```
 
-Now just run all the cells. When you click on the colors, you will be able to see the date of fire (in epoch format) and the number of days before the end_date the fire occured.
+### Method 2 - Run the python script
+This code works similar to the the one present in the notebook, but this doesn't allow any interactions with the map. The output is stored as a southern_california_burn_map.html. You can just open this map on any browser by double clicking it.  
+This is helpful to just grasp an idea of the spread in fire dates across that region and guage how recent or old the fire was. You can change the values of the variables(same as the ones above) in the last part of the code.
+```sh
+#assuming that you already have done the virtual env setup initially
+python3 method2_sce_timesincefire.py
+#this will generate southern_california_burn_map.html and you can open this file in a browser
+```
 
-
-### Sample screenshot
-![screenhot](img.png)
+## Sample outputs
+![method1_sce_timesincefire](SampleOutputs/Method1.png)
---
+++ TimeSinceFire/method2_sce_timesincefire.py
@@ -0,0 +1,126 @@
+import ee
+import folium
+import datetime
+
+# Function to add Earth Engine layers to Folium maps
+def folium_add_ee_layer(map_object, ee_image_object, vis_params, name):
+    tile_url = ee.Image(ee_image_object).getMapId(vis_params)['tile_fetcher'].url_format
+    folium.TileLayer(
+        tiles=tile_url,
+        attr="Google Earth Engine",
+        name=name,
+        overlay=True,
+        control=True
+    ).add_to(map_object)
+
+# Authenticate and initialize Google Earth Engine
+def initialize_ee(project_id):
+    try:
+        ee.Initialize(project=project_id)
+    except ee.EEException:
+        ee.Authenticate()
+        ee.Initialize(project=project_id)
+
+# Function to compute date ranges per year
+def compute_date_ranges(start_date, end_date):
+    start = datetime.datetime.strptime(start_date, "%Y-%m-%d")
+    end = datetime.datetime.strptime(end_date, "%Y-%m-%d")
+
+    # Generate the list of date ranges
+    date_ranges = []
+    current_year = start.year
+
+    while current_year <= end.year:
+        range_start = start_date if current_year == start.year else f"{current_year}-01-01"
+        range_end = end_date if current_year == end.year else f"{current_year}-12-31"
+        date_ranges.append([range_start, range_end])
+        current_year += 1
+
+    return date_ranges
+
+# Function to calculate the number of days between two dates
+def calculate_days(start_date, end_date):
+    start = datetime.datetime.strptime(start_date, "%Y-%m-%d")
+    end = datetime.datetime.strptime(end_date, "%Y-%m-%d")
+    return (end - start).days
+
+# Function to process MODIS data and return the combined image
+def process_modis_data(start_date, end_date, southern_california):
+    reference_date = datetime.datetime.strptime(end_date, "%Y-%m-%d")
+    reference_date_millis = int(reference_date.timestamp() * 1000)
+
+    date_ranges = compute_date_ranges(start_date, end_date)
+
+    def create_tiff_and_extract_data(range_start, range_end):
+        modis_burned_area = ee.ImageCollection('MODIS/061/MCD64A1')\
+            .filter(ee.Filter.date(range_start, range_end))\
+            .filterBounds(southern_california)
+
+        burned_area = modis_burned_area.select('BurnDate')
+        latest_burn = burned_area.reduce(ee.Reducer.max()).rename('latest_burn_date')
+        clipped_burn = latest_burn.clip(southern_california)
+
+        def julian_to_date(image):
+            burn_date = image.select('latest_burn_date')
+            start_of_year_millis = ee.Date(range_start).millis()
+            return burn_date.expression(
+                'burn_date == 0 ? 0 : startOfYear + (burn_date - 1) * 86400000', {
+                    'burn_date': burn_date,
+                    'startOfYear': start_of_year_millis
+                }).rename("yearly_burn_date")
+
+        return julian_to_date(clipped_burn).toFloat()
+
+    most_recent_burn = ee.Image(0).rename('most_recent_burn_date').clip(southern_california).toFloat()
+
+    for range_start, range_end in date_ranges:
+        yearly_burn_date = create_tiff_and_extract_data(range_start, range_end)
+        most_recent_burn = most_recent_burn.where(
+            yearly_burn_date.gt(most_recent_burn), yearly_burn_date
+        )
+
+    days_since_burn = most_recent_burn.expression(
+        'burn_date == 0 ? 0 : round((reference_date - burn_date) / (1000 * 60 * 60 * 24))', {
+            'reference_date': reference_date_millis,
+            'burn_date': most_recent_burn
+        }
+    ).rename("days_since_last_burn").toFloat()
+
+    return most_recent_burn.addBands(days_since_burn)
+
+# Function to visualize the processed data
+def visualize_burn_map(combined_image, start_date, end_date, southern_california, palette, map_center):
+    max_days = calculate_days(start_date, end_date)
+    vis_params = {
+        'bands': 'days_since_last_burn',
+        'min': 1,
+        'max': max_days,
+        'palette': palette
+    }
+
+    m = folium.Map(location=map_center, zoom_start=7)
+
+    masked_combined_image = combined_image.updateMask(combined_image.select('days_since_last_burn').neq(0))
+    folium_add_ee_layer(m, masked_combined_image, vis_params, "Days Since Last Burn")
+    folium.LayerControl().add_to(m)
+
+    output_html = "southern_california_burn_map.html"
+    m.save(output_html)
+
+    print(f"Map saved to {output_html}. Open this file in your browser to view the map.")
+
+
+########### Define values as per you needs ###########
+initialize_ee("test-09112024") #google earth engine project id
+start_date = "2000-01-01"
+end_date = "2024-09-30"
+southern_california = ee.Geometry.BBox(-120.708008, 33.156797, -113.975687, 38.395343)
+palette = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
+map_center = [35.5688, -118.8678]
+
+########### End variable definitions ###########
+
+
+# Process data and visualize
+combined_image = process_modis_data(start_date, end_date, southern_california)
+visualize_burn_map(combined_image, start_date, end_date, southern_california, palette, map_center)
